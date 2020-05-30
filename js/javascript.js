4dba39802fb7296ffd225a09ed45c1188d07fe43
Joshua <jwon648@aucklanduni.ac.nz>
2020-05-30 23:01:56 +1200

    Fixed JS for slideshow, and started program descriptions

--- js/javascript.js
+++ js/javascript.js
@@ -2,40 +2,40 @@ const PROGRAMS = [
 	{
 		id: "program-1",
 		program_title: "France",
-		program_subtitle: "Exchange program to France",
-		program_description: 'Description about exchange program to France<p> <span id="dots1"></span><span id="more1">Lots of info about the program-Lots of info about the program-Lots of info about the program-</span></p> <button onclick="myFunction(1)" id="myBtn1">Read more</button>',
+		program_subtitle: "Exchange program to Paris",
+		program_description: 'This is an exchange trip that takes a student to Paris for a 1-6 month internship.<p> <span id="dots1"></span><span id="more1">Features: <br> Become integrated into a work environment in Paris, working in a field of your choosing including fashion and design! <br> Learn French <br> Explore Paris and other cities in France!</span></p> <button onclick="myFunction(1)" id="myBtn1">Read more</button>',
 		image: "images/france.jpg",
 		image_title: "Paris"
 	},	
 	{
 		id: "program-2",
 		program_title: "Spain",
-		program_subtitle: "Exchange program to Spain",
-		program_description: 'Description about exchange program to Spain<p> <span id="dots2"></span><span id="more2">More info here</span></p> <button onclick="myFunction(2)" id="myBtn2">Read more</button>',
+		program_subtitle: "Exchange program to Barcelona",
+		program_description: 'This is an exchange trip that takes a student to Barcelona for a 1-6 month internship.<p> <span id="dots2"></span><span id="more2">More info here</span></p> <button onclick="myFunction(2)" id="myBtn2">Read more</button>',
 		image: "images/spain.jpg",
 		image_title: "Barcelona"
 	},
 	{
 		id: "program-3",
 		program_title: "Germany",
-		program_subtitle: "Exchange program to Germany",
-		program_description: 'Description about exchange program to Germany<p> <span id="dots3"></span><span id="more3">More info here</span></p> <button onclick="myFunction(3)" id="myBtn3">Read more</button>',
+		program_subtitle: "Exchange program to Berlin",
+		program_description: 'This is an exchange trip that takes a student to Berlin for a 1-6 month internship.<p> <span id="dots3"></span><span id="more3">More info here</span></p> <button onclick="myFunction(3)" id="myBtn3">Read more</button>',
 		image: "images/germany.jpg",
 		image_title: "Berlin"
 	},
 	{
 		id: "program-4",
 		program_title: "Russia",
-		program_subtitle: "Exchange program to Russia",
-		program_description: 'Description about exchange program to Russia<p> <span id="dots4"></span><span id="more4">More info here</span></p> <button onclick="myFunction(4)" id="myBtn4">Read more</button>',
+		program_subtitle: "Exchange program to Moscow",
+		program_description: 'This is an exchange trip that takes a student to Moscow for a 1-6 month internship.<p> <span id="dots4"></span><span id="more4">More info here</span></p> <button onclick="myFunction(4)" id="myBtn4">Read more</button>',
 		image: "images/russia.jpg",
 		image_title: "Moscow"
 	},
 	{
 		id: "program-5",		
 		program_title: "USA",
-		program_subtitle: "Exchange program to USA",
-		program_description: 'Description about exchange program to USA<p> <span id="dots5"></span><span id="more5">More info here</span></p> <button onclick="myFunction(5)" id="myBtn5">Read more</button>',
+		program_subtitle: "Exchange program to New York",
+		program_description: 'This is an exchange trip that takes a student to New York for a 1-6 month internship.<p> <span id="dots5"></span><span id="more5">More info here</span></p> <button onclick="myFunction(5)" id="myBtn5">Read more</button>',
 		image: "images/usa.jpg",
 		image_title: "New York"
 	},
@@ -44,8 +44,8 @@ const PROGRAMS = [
 	{
 		id: "program-6",		
 		program_title: "Canada",
-		program_subtitle: "Exchange program to Canada",
-		program_description: 'Description about exchange program to Canada<p> <span id="dots6"></span><span id="more6">More info here</span></p> <button onclick="myFunction(6)" id="myBtn6">Read more</button>',
+		program_subtitle: "Exchange program to Vancouver",
+		program_description: 'This is an exchange trip that takes a student to Vancouver for a 1-6 month internship.<p> <span id="dots6"></span><span id="more6">More info here</span></p> <button onclick="myFunction(6)" id="myBtn6">Read more</button>',
 		image: "images/canada.jpg",
 		image_title: "Vancouver"
 	},
@@ -53,8 +53,8 @@ const PROGRAMS = [
 	{
 		id: "program-7",		
 		program_title: "Japan",
-		program_subtitle: "Exchange program to Japan",
-		program_description: 'Description about exchange program to Japan<p> <span id="dots7"></span><span id="more7">More info here</span></p> <button onclick="myFunction(7)" id="myBtn7">Read more</button>',
+		program_subtitle: "Exchange program to Tokyo",
+		program_description: 'This is an exchange trip that takes a student to Tokyo for a 1-6 month internship.<p> <span id="dots7"></span><span id="more7">More info here</span></p> <button onclick="myFunction(7)" id="myBtn7">Read more</button>',
 		image: "images/japan.jpg",
 		image_title: "Akihabara"
 	},
@@ -177,17 +177,6 @@ function showSlides(n) {
 
 
 mybutton = document.getElementById("myBtn");
-
-
-window.onscroll = function() {scrollFunction()};
-
-function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    mybutton.style.display = "block";
-  } else {
-    mybutton.style.display = "none";
-  }
-}
 
 
 function topFunction() {
