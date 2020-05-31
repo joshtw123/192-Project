2d253003ec634803029fd45b9977518587b2e75b
Joshua <jwon648@aucklanduni.ac.nz>
2020-06-01 11:27:39 +1200

    7 image auto slideshow implemented

--- js/javascript.js
+++ js/javascript.js
@@ -174,6 +174,22 @@ function showSlides(n) {
 }
 
 
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 3000);
+}
+
+
 
 mybutton = document.getElementById("myBtn");
 
